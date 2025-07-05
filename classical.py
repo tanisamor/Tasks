6c7cca1766c9f75c3797b9523de8bfe469b9429f
tanisamor <119941819+tanisamor@users.noreply.github.com>
2025-07-06 02:47:13 +0500

    Update classical.py

--- classical.py
+++ classical.py
@@ -3,16 +3,13 @@ import matplotlib.pyplot as plt
 # Parameters( Please note, some values are supposed according to the situation)
 
 c = 1.0          #speed
-
 L = 1.0           # length along the x-direction
-
 T = 0.1           # total time
 N = 10            # number of spatial grid points (excluding boundaries)
 dx = L / (N + 1)  # spatial step size
 dt = 0.01         # time step
 lambda_val = c * dt / dx
 print("λ = ", lambda_val)
-
 
 A = np.zeros((N, N))
 for i in range(N):
@@ -24,15 +21,12 @@ for i in range(N):
 print("Matrix A:")
 print(A)
 
-
 x = np.linspace(dx, L - dx, N)
 u_j = np.sin(np.pi * x)
 print("Initial u^j:")
 print(u_j)
 
-
 u_next = np.linalg.solve(A, u_j)
-
 
 plt.plot(x, u_j, label="u^j (initial)")
 plt.plot(x, u_next, label="u^{j+1} (after one step)")
